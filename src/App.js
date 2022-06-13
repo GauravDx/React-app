43ead9472be6895b915261c95ca7dee8e7ff7187
Gaurav Kumar <202686@cognizant.comm>
2022-06-14 00:24:58 +0530

    "Secont Commit"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+const App = () => {
+    console.log("Appssssssss")
+    return (
+        <div className="App">
+            This is the test inside the App.js
+        </div>
+    );
+};
+
+export default App;
